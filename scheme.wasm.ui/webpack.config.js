da1f12514a8cd8eb6d6d3a4c9a54a59b20f296aa
Paul C Roberts <pacaro@gmail.com>
2021-12-11 17:11:04 -0800

    hello world

---
+++ scheme.wasm.ui/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ 
+module.exports = (env, argv) => {
+  // Selects a different template file for the start page based on whether
+  // this is a "production" build or not
+  const isProduction = argv.mode === "production"
+  const index_template = isProduction ? "./index.prod.html"
+                                      : "./index.dev.html";
+ 
+  return {
+    entry: './src/index.tsx', // The entry point into the bundle
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: '[name].[contenthash].js' // The name of the output bundle,
+                                          // this is referenced from the start
+                                          // page.
+    },
+    devServer: {
+      port: 8080,
+      host: '0.0.0.0',
+      static: {
+        publicPath: path.join(__dirname, '/dist'),
+      },
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js']
+    },
+ 
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: 'ts-loader'
+        },
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          loader: 'source-map-loader'
+        }
+      ]
+    },
+ 
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM'
+    },
+ 
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'scheme.wasm UI',    // Title is injected into the page template
+        filename: 'scheme.wasm.html', // The output name when built
+        template: index_template
+      })
+    ]
+  };
+};
